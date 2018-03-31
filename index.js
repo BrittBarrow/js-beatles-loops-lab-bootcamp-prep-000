3d86190b70a2ed4ecc119b9f2c2373cc340fe968
Brittany Barrow <bangelabarrow@gmail.com>
2018-03-31 03:39:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -34,10 +34,9 @@ function iLoveTheBeatles(number) {
   // The condition of the loop should check to see that the parameter number is less than 15.
   // The function should return the array with the strings "I love the Beatles!".
   var empty =[]
-  var i=0
   do {
     empty.push("I love the Beatles!");
-    i++;
-  } while (i<15);
+    number++;
+  } while (number<15);
   return empty;
 }
